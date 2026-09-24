d85fd2aa05e3ce32615fb5ca5a703e914b228a98
KANAPARTHI VAMSI KRISHNA <vamsikanaparthi09@gmail.com>
2026-09-24 16:28:21 +0530

    Delete layoutproject2 directory
    deleted

--- layoutproject2/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import student
-
-class studentForm(forms.ModelForm):
-    class Meta:
-        model = student
-        fields = '__all__'
--- layoutproject2/myapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class student(models.Model):
-    studentid = models.IntegerField()
-    studentname = models.CharField(max_length=40)
-    studentfee = models.IntegerField()
